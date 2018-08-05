c9099be2ad0f156c581e827db980af398135bf3f
kiralacus <1064655497@qq.com>
2018-08-05 16:23:50 +0800

    finish the part of ordercode without debug at the backend

--- tornado/ihome_tornado/Handler/OrderHandler.py
+++ tornado/ihome_tornado/Handler/OrderHandler.py
@@ -7,16 +7,16 @@ import datetime
 
 import logging
 
-from utils.commons import require_auth,require_login
+from utils.commons import require_login, require_auth
 
-class OrderBookHandler(BaseHandler):
+class BookOrderHandler(BaseHandler):
     '''订单信息存储'''
     @require_login
     def post(self):
         start_date = self.json_dict.get('start_date')
         end_date = self.json_dict.get('end_date')
         house_id = self.json_dict.get('house_id')
-        # user_id = 3
+        user_id = self.get_current_user().get('userId')
         if not all((start_date, end_date, house_id)):
             return self.write(dict(errcode=RET.PARAMERR, errmsg='miss the param'))
         if start_date > end_date:
@@ -37,6 +37,7 @@ class OrderBookHandler(BaseHandler):
         capacity = int(house['hi_capacity'])
         price = int(house['hi_price'])
         deposit = int(house['hi_deposit'])
+        amount = price*delta + deposit
         if not(delta >= min_days and delta <= max_days):
             if delta < min_days:
                 return self.write(dict(errcode=RET.DATAERR, errmsg='住房时间过短'))
@@ -49,11 +50,129 @@ class OrderBookHandler(BaseHandler):
             try:
                 sql = 'insert into ih_order_info(oi_user_id, oi_house_id, oi_begin_date, oi_end_date, oi_days, oi_house_price, oi_amount)' \
                       'values(%(user_id)s, %(house_id)s, %(begin_date)s, %(end_date)s, %(days)s, %(house_price)s, %(amount)s);'
-                self.db.execute(sql, user_id=user_id, house_id=house_id, begin_date=start_date, end_date=end_date, house_price=price, days=delta, amount=deposit)
+                self.db.execute(sql, user_id=user_id, house_id=house_id, begin_date=start_date, end_date=end_date, house_price=price, days=delta, amount=amount)
             except Exception as e:
                 logging.error(e)
                 return self.write(dict(errcode=RET.DBERR, errmsg='fail in insert data'))
             return self.write(dict(errcode=RET.OK, errmsg='成功', total_price=total_price))
+
+
+class MyOrderHandler(BaseHandler):
+    '''處理我的訂單內容'''
+    @require_login
+    def get(self):
+        '''
+        :return:
+        param 1.order_id 2.order_status 3.image_url 4.order_title 5.order_ctime 6.order_start_date 7.order_end_date 8.order_amount 9.order_days 10. order_comment
+        '''
+        user_id = self.get_current_user().get('userId')
+        try:
+            sql = 'select oi_order_id, oi_begin_date, oi_status, oi_end_date, oi_days, oi_ctime, oi_house_price, hi_title, hi_index_image_url, oi_comment from ih_order_info ' \
+                  'inner join ih_house_info on ih_order_info.oi_house_id = ih_house_info.hi_house_id where oi_user_id=%(user_id)s'
+            order_info = self.db.query(sql, user_id=user_id)
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(errcode=RET.DBERR, errmsg='数据库查询订单信息出错'))
+        order = []
+        for each in order_info:
+            order_dict = {}
+            order_dict['order_id'] = each['oi_order_id']
+            order_dict['start_date'] = each['oi_begin_date']
+            order_dict['end_date'] = each['oi_end_date']
+            order_dict['status'] = each['oi_status']
+            order_dict['ctime'] = each['oi_ctime']
+            order_dict['days'] = each['oi_days']
+            order_dict['title'] = each['hi_title']
+            order_dict['img_url'] = each['hi_index_image_url']
+            order_dict['amount'] = each['oi_house_price']
+            order_dict['comment'] = each['oi_comment']
+            order.append(order_dict)
+        return self.write(dict(errcode=RET.OK, errmsg='成功', order=order))
+
+    def post(self):
+        '''提交评论内容'''
+        order_id = self.json_dict.get('order_id')
+        comment = self.json_dict.get('comment')
+        if not all((comment, order_id)):
+            return self.write(dict(errcode=RET.PARAMERR, errmsg='缺少参数'))
+        try:
+            sql = 'insert into ih_order_info(oi_comment) values(%(comment)s) where oi_order_id=%(order_id);'
+            self.db.execute(sql, comment=comment, order_id=order_id)
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(errcode=RET.DBERR, errmsg='向数据库写入数据出错'))
+        return self.write(dict(errcode=RET.OK, errmsg='成功'))
+
+class GuestOrderHandler(BaseHandler):
+    @require_login
+    @require_auth
+    def get(self):
+        '''
+        oi_order_id-> order_id
+        oi_status -> status
+        hi_title -> title
+        oi_ctime -> ctime
+        oi_begin_date -> begin_date
+        oi_end_date -> end_date
+        oi_amount -> amount
+        oi_days -> days
+        oi_comment -> comment
+        :return:
+
+        '''
+        user_id = self.get_current_user().get('userId')
+        try:
+            sql = 'select oi_order_id, oi_status, hi_title, oi_ctime, oi_begin_date, oi_end_date, oi_amount, oi_days, oi_comment from ih_house_info ' \
+                  'inner join ih_order_info on ih_order_info.oi_house_id=ih_house_info.hi_house_id where hi_user_id=%(user_id)s;'
+            order_info = self.db.query(sql, user_id=user_id)
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(errcode=RET.DBERR, errmsg='订单数据查询错误'))
+
+        order = []
+        for each in order_info:
+            order_dict = {}
+            order_dict['order_id'] = each['oi_order_id']
+            order_dict['status'] = each['oi_status']
+            order_dict['title'] = each['hi_title']
+            order_dict['ctime'] = each['oi_ctime']
+            order_dict['begin_date'] = each['oi_begin_date']
+            order_dict['end_date'] = each['oi_end_date']
+            order_dict['amount'] = each['oi_amount']
+            order_dict['days'] = each['oi_days']
+            order_dict['comment'] = each['oi_comment']
+            order.append(order_dict)
+
+        return self.write(dict(errcode=RET.OK, errmsg='成功', order=order))
+
+    def post(self):
+        order_id = self.json_dict.get('order_id')
+        comment = self.json_dict.get('comment')
+        if not all((order_id, comment)):
+            return self.write(dict(errcode=RET.PARAMERR, errmsg='缺少参数'))
+        try:
+            sql = 'insert into ih_order_info(oi_comment) values(%(comment)s) where oi_order_id=%(order_id)s'
+            self.db.execute(sql, order_id=order_id, comment=comment)
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(errcode=RET.DBERR, errmsg='数据库写入失败'))
+        return self.write(dict(errcode=RET.OK, errmsg='成功'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
